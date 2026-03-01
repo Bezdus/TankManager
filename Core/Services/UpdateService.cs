498b3d03470c5ede4b72cd267a654350a9537ff7
Sergey <Cepbiu.98@gmail>
2026-03-01 13:22:23 +0300

    Обновление до версии 1.0.3.0 и улучшение автообновления
    Внесены изменения для поддержки версии 1.0.3.0: обновлены номера версии в AssemblyInfo.cs и update.xml. В UpdateService.cs теперь при наличии обновления отображается форма обновления через AutoUpdater.ShowUpdateForm(args).

--- Core/Services/UpdateService.cs
+++ Core/Services/UpdateService.cs
@@ -66,7 +66,7 @@ namespace TankManager.Core.Services
             {
                 if (args.IsUpdateAvailable)
                 {
-                    // Обновление доступно - AutoUpdater покажет диалог автоматически
+                    AutoUpdater.ShowUpdateForm(args);
                 }
                 else
                 {
